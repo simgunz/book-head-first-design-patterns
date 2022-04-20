448ced13ac4775e41704169b8934b037a7501a17
Simone Gaiarin <simgunz@gmail.com>
2022-04-20 11:12:31 +0200

    Add statistics display to weather station

--- hfdesignpatterns/observer/weatherstation.py
+++ hfdesignpatterns/observer/weatherstation.py
@@ -1,10 +1,11 @@
-from displays import CurrentConditionsDisplay
+from displays import CurrentConditionsDisplay, StatisticsDisplay
 from weatherdata import WeatherData
 
 
 def main():
     weather_data = WeatherData()
     current_conditions_display = CurrentConditionsDisplay(weather_data)
+    statistics_display = StatisticsDisplay(weather_data)
 
     weather_data.set_measurements(80, 65, 30.4)
     weather_data.set_measurements(82, 70, 29.2)
