d2ac49b1065e868ceacc43a9635fce5b8370a5a8
Simone Gaiarin <simgunz@gmail.com>
2022-04-20 11:12:15 +0200

    Add statistics display

--- hfdesignpatterns/observer/displays.py
+++ hfdesignpatterns/observer/displays.py
@@ -26,3 +26,25 @@ class CurrentConditionsDisplay(Observer, DisplayElement):
             f"Current conditions: {self.temperature}F degrees and {self.humidity}%"
             " humidity"
         )
+
+
+class StatisticsDisplay(Observer, DisplayElement):
+    def __init__(self, weather_data):
+        self.temperatures = []
+        self.min_temperature = 1000
+        self.max_temperature = -1000
+        self._weather_data = weather_data
+        self._weather_data.register_observer(self)
+
+    def update(self, temperature, humidity, pressure):
+        self.temperatures.append(temperature)
+        self.min_temperature = min(self.min_temperature, temperature)
+        self.max_temperature = max(self.max_temperature, temperature)
+        self.display()  # Not so nice to call display here
+
+    def display(self):
+        print(
+            "Avg/Max/Min temperature ="
+            f" {sum(self.temperatures) / len(self.temperatures)}"
+            "/{self.max_temperature}/{self.min_temperature}"
+        )
