79abd31f6b457ffadafab14e72b1c3edc86b3b93
Simone Gaiarin <simgunz@gmail.com>
2022-04-20 09:54:49 +0200

    Add subject interface

---
+++ hfdesignpatterns/observer/observer.py
@@ -0,0 +1,16 @@
+import abc
+
+
+class Subject(abc.ABC):
+    def __init__(self):
+        self.observers = set()
+
+    def register_observer(self, observer):
+        self.observers.add(observer)
+
+    def remove_observer(self, observer):
+        self.observers.remove(observer)
+
+    @abc.abstractmethod
+    def notify_observers(self):
+        pass
