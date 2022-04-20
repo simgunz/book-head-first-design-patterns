641bb32d76a6aaf3a209bc31e253fc6807405f3c
Simone Gaiarin <simgunz@gmail.com>
2022-04-20 10:53:38 +0200

    Add implementation of current conditions display

--- hfdesignpatterns/observer/displays.py
+++ hfdesignpatterns/observer/displays.py
@@ -1,7 +1,26 @@
 import abc
 
+from observer import Observer
+
 
 class DisplayElement(abc.ABC):
     @abc.abstractmethod
     def display(self):
         raise NotImplementedError()
+
+
+class CurrentConditionsDisplay(Observer, DisplayElement):
+    def __init__(self):
+        self.temperature = None
+        self.humidity = None
+
+    def update(self, temperature, humidity, pressure):
+        self.temperature = temperature
+        self.humidity = humidity
+        self.display()
+
+    def display(self):
+        print(
+            f"Current conditions: {self.temperature}F degrees and {self.humidity}%"
+            " humidity"
+        )
