26ed53fac8489d22538146f6a953ed372d4df8bc
Simone Gaiarin <simgunz@gmail.com>
2022-04-22 11:01:32 +0200

    Fix design of Condiment decorator

--- hfdesignpatterns/decorator/condiments.py
+++ hfdesignpatterns/decorator/condiments.py
@@ -1,14 +1,6 @@
-import abc
+from bevarages import Bevarage
 
 
-class CondimentDecorator(abc.ABC):
+class CondimentDecorator(Bevarage):
     def __init__(self, bevarage):
         self._bevarage = bevarage
-
-    @abc.abstractmethod
-    def get_description(self):
-        raise NotImplementedError()
-
-    @abc.abstractmethod
-    def cost(self):
-        raise NotImplementedError()
