34592e3b87061386556de3fbcf5af64882567dbf
Simone Gaiarin <simgunz@gmail.com>
2022-04-22 11:16:04 +0200

    Add concrete condiments

--- hfdesignpatterns/decorator/condiments.py
+++ hfdesignpatterns/decorator/condiments.py
@@ -10,3 +10,27 @@ class CondimentDecorator(Bevarage):
     @abc.abstractmethod
     def get_description(self):
         raise NotImplementedError()
+
+
+class Mocha(CondimentDecorator):
+    def get_description(self):
+        return super().get_description() + ", Mocha"
+
+    def cost(self):
+        return super().cost() + 0.20
+
+
+class Soy(CondimentDecorator):
+    def get_description(self):
+        return super().get_description() + ", Soy"
+
+    def cost(self):
+        return super().cost() + 0.15
+
+
+class Whip(CondimentDecorator):
+    def get_description(self):
+        return super().get_description() + ", Whip"
+
+    def cost(self):
+        return super().cost() + 0.10
