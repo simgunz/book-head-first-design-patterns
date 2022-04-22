d65e5d8f5386529a2c16a57fc0b228a3129d08ba
Simone Gaiarin <simgunz@gmail.com>
2022-04-22 11:11:17 +0200

    Force all condiment decorators to reimplement the description method

--- hfdesignpatterns/decorator/condiments.py
+++ hfdesignpatterns/decorator/condiments.py
@@ -1,6 +1,12 @@
+import abc
+
 from bevarages import Bevarage
 
 
 class CondimentDecorator(Bevarage):
     def __init__(self, bevarage):
         self._bevarage = bevarage
+
+    @abc.abstractmethod
+    def get_description(self):
+        raise NotImplementedError()
